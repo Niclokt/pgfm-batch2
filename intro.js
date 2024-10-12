182a16db395ec72be512732a0b9342794de02f80
Niclokt <nicolelowkt@gmail.com>
2024-10-12 16:35:48 +0800

    more details to conditional loops

--- intro.js
+++ intro.js
@@ -5,3 +5,25 @@
 
 //declare variable
 
+//if-else conditions
+
+
+//for loops
+//Params: (1) Decalare, (2)Conditions, (3)iterator
+for (var i = 1; i<10 ; i++){
+    //continually do something, until the condition in parameter is met
+}
+
+//While loops
+while (ships > 5){
+    ships = ships - xAttack
+}
+
+// Stacking of cards = Macros in excel OR Functions in programming
+//
+function attackShip(){
+    for () {
+        
+    }
+
+}
