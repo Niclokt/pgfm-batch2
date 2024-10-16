f1ee232dd5a10d672ba82f4891df5a16f5d8b587
Niclokt <nicolelowkt@gmail.com>
2024-10-16 23:43:12 +0800

    included tutorial

--- Tutorial_01_16OCT2024_Nicole.js
+++ Tutorial_01_16OCT2024_Nicole.js
@@ -1,1 +1,18 @@
 //Exercise 01
+console.log("##Exercise 01");
+let p1 = 53;
+let p2 = Boolean(53);
+let p3 = "53";
+
+console.log(typeof(p1));
+
+
+//Exercise 03
+let x = 6;
+let y = 2;
+
+console.log(`X * Y = ${x * y}`);
+console.log(`X / Y = ${x / y}`);
+console.log(`X - Y = ${x - y}`);
+console.log(`X + Y = ${x * y}`);
+console.log(`X % Y = ${x % y}`);
