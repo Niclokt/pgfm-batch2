518ec93fa4ce1cb41f00c69b4daa1b3d661201f4
Niclokt <nicolelowkt@gmail.com>
2024-10-19 13:40:00 +0800

    Include Tutorial 02 (Lesson 3)

--- Tutorial_01_16OCT2024_Nicole.js
+++ Tutorial_01_16OCT2024_Nicole.js
@@ -5,9 +5,12 @@ let p2 = Boolean(53);
 let p3 = "53";
 
 console.log(typeof(p1));
+console.log(typeof(p2));
+console.log(typeof(p3));
 
 
 //Exercise 03
+console.log("##Exercise 03");
 let x = 6;
 let y = 2;
 
@@ -16,3 +19,5 @@ console.log(`X / Y = ${x / y}`);
 console.log(`X - Y = ${x - y}`);
 console.log(`X + Y = ${x * y}`);
 console.log(`X % Y = ${x % y}`);
+
+//Exercise 04 
