6cd440cca81214052bb9ca64aae55e722385e46b
Niclokt <nicolelowkt@gmail.com>
2024-10-19 14:21:52 +0800

    included tutorial 03

--- Tutorial_02_19OCT2024_Nic.js
+++ Tutorial_02_19OCT2024_Nic.js
@@ -1,3 +1,35 @@
-//For Loop 
+// //For Loop 
+
+// for (let i = 0; i< 10;i++){
+//     console.log("Practise:" + i);
+// }
+
+// //While Loop
+// let i = 0; 
+// while(i < 10){
+
+// }
+
+
+//Functions
+//Parameters used in functions, are defined by LOCAL variables
+//simple functions, don't return anything, but maybe just process
+//Example
+
+let valueA = 30;
+let valueB = 20;
+
+//call the function 
+let sum = sumOfNumbers (valueA, valueB);
+
+function sumOfNumbers (num1, num2){
+    total = num1 + num2;
+
+    console.log(`Sum =  ${total}`);
+    return total;
+}
+
+
+
 
 
---
+++ Tutorial_03_19OCT2024_Nic.js
@@ -0,0 +1,12 @@
+//Tutorial03 
+
+
+//Exercise 3 
+
+//Exercise 5 
+
+
+
+//Exercise 6 
+
+//Exercise 7 
