344a713083f2d2b6203af7d276378e676d1f5a36
Niclokt <nicolelowkt@gmail.com>
2024-10-19 16:42:14 +0800

    tutorial01_ex18

--- Tutorial_01_16OCT2024_Nicole.js
+++ Tutorial_01_16OCT2024_Nicole.js
@@ -1,5 +1,5 @@
 //Exercise 01
-console.log("##Exercise 01");
+/* console.log("##Exercise 01");
 let p1 = 53;
 let p2 = Boolean(53);
 let p3 = "53";
@@ -7,10 +7,10 @@ let p3 = "53";
 console.log(typeof(p1));
 console.log(typeof(p2));
 console.log(typeof(p3));
-
+ */
 
 //Exercise 03
-console.log("##Exercise 03");
+/* console.log("###EXERCISE 03###");
 let x = 6;
 let y = 2;
 
@@ -19,9 +19,35 @@ console.log(`X / Y = ${x / y}`);
 console.log(`X - Y = ${x - y}`);
 console.log(`X + Y = ${x * y}`);
 console.log(`X % Y = ${x % y}`);
-
+ */
 //Exercise 04 
 
 
 
 //Exercise 18 
+console.log('###EXERCISE 18###');
+//this is user driven
+let isSunny = false;
+let isCloudy = true;
+let isEveGoing = true;
+let isTuesday = false;
+let isJaniceGoing = false;
+
+//this is the rule
+if ((isSunny||isCloudy)&& isEveGoing && isTuesday!== true && isJaniceGoing !== true){
+    console.log('Going to the beach!')
+}else {
+    console.log("Not going to the beach");
+}
+
+
+//--- ChatGPT 18 ---
+/* let isSunny = false;
+let isCloudy = true;
+let isEveGoing = true;
+let isTuesday = false;
+let isJaniceGoing = false;
+
+const canGoToBeach = (isSunny || isCloudy) && isEveGoing && !isTuesday && !isJaniceGoing;
+
+console.log(canGoToBeach ? 'Going to the beach!' : 'Not going to the beach'); */
