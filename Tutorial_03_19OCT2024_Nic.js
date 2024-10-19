2f1e70d761eb4f15290d44bc833e4b9221793c52
Niclokt <nicolelowkt@gmail.com>
2024-10-19 14:50:12 +0800

    added solutions to tutorial 03

--- Tutorial_03_19OCT2024_Nic.js
+++ Tutorial_03_19OCT2024_Nic.js
@@ -1,12 +1,100 @@
 //Tutorial03 
 
+//**EXERCISE 1**//
+// Objective
+// Learn how to add elements to an array using various methods.
+// Instructions:
+// ● Create an array named fruits and initialize it with some fruit names.
+// ● Add "mango" to the end of the array using the push() method.
+// ● Add "strawberry" to the beginning of the array using the unshift() method.
+// ● Print the final array to the console.
+console.log('###EXERCISE 01###');
 
-//Exercise 3 
-
-//Exercise 5 
+//Declare function
 
 
 
-//Exercise 6 
 
-//Exercise 7 
+//**EXERCISE 3**// 
+
+
+
+//**EXERCISE 5**// 
+// Objective
+// Write a function that calculates the area of a rectangle.
+// Instructions
+// Define a function named calculateArea that takes two parameters: width and height. Inside the function, compute the area as width * height and return the result.
+// Call the function with different sets of values to test it and print the results.
+
+console.log('###EXERCISE 05###');
+//Declare function
+function calculateArea (width, height){
+    area = width * height;
+    console.log(`Area of Rectangle = ${area}`);
+}
+
+//Calling functions
+let width1 = 10;
+let height1 = 3;
+calculateArea(width1,height1);
+
+let width2 = 12;
+let height2 = 7;
+calculateArea(width2,height2);
+
+
+
+//**EXERCISE 6**//
+// Objective: Write a function to categorize a person's age into child, teen, or adult.
+// Instructions
+// ● Define a function named checkAge that takes one parameter: age.
+// ● Use conditional statements to categorize the age and return the category.
+// ● Print the result based on the age.
+
+
+console.log('###EXERCISE 06###');
+//Declare function
+function checkAge(age){
+    
+    console.log(
+        age >= 70 ? "You are an elderly." : 
+        age >= 40 ? "You are a middle aged person." : 
+        age >= 20 ? "You are a young adult." : 
+        age >= 13 ? "You are a teenager." : 
+        "You are a kid."
+      );
+}
+
+//Calling function
+let age1 = 30;
+checkAge(age1);
+
+let age2 = 3;
+checkAge(age2);
+
+
+
+
+//**EXERCISE 07**//
+// Objective
+// Create a function that converts temperatures from Celsius to Fahrenheit.
+// Instructions
+// ● Define a function named celsiusToFahrenheit that takes one parameter: celsius.
+// ● Convert the Celsius temperature to Fahrenheit using the formula (celsius * 9/5) + 32.
+// ● Return the Fahrenheit temperature.
+// ● Call the function and print the output for several Celsius values.
+
+console.log('###EXERCISE 07###');
+//Declare a function
+function celsiusToFahrenheit (celsius){
+
+    farenheit = celsius * 95;
+    console.log(`${celsius} Celsius = ${farenheit} Farenheit`);
+}
+
+//Call function 
+let celcius1 = 10;
+celsiusToFahrenheit(celcius1);
+
+let celcius2 = 40;
+celsiusToFahrenheit(celcius2);
