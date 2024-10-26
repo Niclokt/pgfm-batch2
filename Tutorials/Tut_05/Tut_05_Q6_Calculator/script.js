5bb5f7601d967c5e48c4feabcd78c476cddadf77
Niclokt <nicolelowkt@gmail.com>
2024-10-26 19:58:37 +0800

    tut 05 - q6 updated

--- Tutorials/Tut_05/Tut_05_Q6_Calculator/script.js
+++ Tutorials/Tut_05/Tut_05_Q6_Calculator/script.js
@@ -1,1 +1,9 @@
-//
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+let sumResult = document.getElementById('sumResult')
+
+btn_getSum.addEventListener('click', function(){
+    let sum = parseInt(document.getElementById('input1').value) + parseInt(document.getElementById('input2').value);
+    sumResult.innerText = sum;
+    alert(`Input1 + Input2 = ${sum}`);
+});
