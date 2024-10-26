6823f94a1fbce2f8216b3770f0dfaa826b20e3ec
Niclokt <nicolelowkt@gmail.com>
2024-10-26 19:24:58 +0800

    L5_MockTest_Q9 - Completed

--- L5_MockTest/MockTest_Q9.js
+++ L5_MockTest/MockTest_Q9.js
@@ -2,4 +2,25 @@
 // // Example usage:
 // console.log(sumArray([1, 2, 3, 4])); // Should return 10
 
+const arr = [1,2,3,4,6];
 
+
+function sumArray(array){
+    //get length of array
+    arrLength = array.length;
+    console.log(`Length of array = ${arrLength}`);
+
+    //get sum of elements in array
+    let arrSum = 0;
+    for (let i=0; i < arrLength; i++){
+    // (wrong) for (let i=0; i < array.length; i++){
+        arrSum += array.pop();
+        console.log(`i = ${i} & arrSum = ${arrSum}`);
+    }
+
+    //return / print sum
+    console.log(arrSum);
+    return arrSum;
+}
+
+sumArray(arr);
